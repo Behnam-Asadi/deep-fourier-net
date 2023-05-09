28908faabfa20976e461adf36804ff5c91c855f0
Behnam-Asadi <38032617+Behnam-Asadi@users.noreply.github.com>
2023-05-09 18:41:47 -0400

    Update network_forward_pass.py

--- network_forward_pass.py
+++ network_forward_pass.py
@@ -3,72 +3,57 @@ from jax import jit, vmap
 from functools import partial
 import jax
 
+def initialize_embedding(sample_size=3033042, output_dim=120):
+    global key
+    key, rng = jax.random.split(key)
+    embedding = 1 * random.normal(rng, shape=(sample_size, output_dim))
+    return embedding
 
+@jit
+def f_layer(params, x):
+    return np.dot(params[0], np.cos(2 * np.pi * np.dot(params[2], x))) + np.dot(params[1], np.sin(
+        2 * np.pi * np.dot(params[2], x)))
 
-# def initialize_embedding(sample_size=3033042, output_dim=120):
-#     global key
-#     key, rng = jax.random.split(key)
-#     embedding = 1 * random.normal(rng, shape=(sample_size, output_dim))
-#     return embedding
-
-# @jit
-# def f_layer(params, x):
-#     return np.dot(params[0], np.cos(2 * np.pi * np.dot(params[2], x))) + np.dot(params[1], np.sin(
-#         2 * np.pi * np.dot(params[2], x)))
-#
-#
-# @jit
-# def f_res_layer(params, x):
-#     return np.dot(params[0], np.cos(2 * np.pi * np.dot(params[2], x))) + np.dot(params[1], np.sin(
-#         2 * np.pi * np.dot(params[2], x))) + x
-
+@jit
+def f_res_layer(params, x):
+    return np.dot(params[0], np.cos(2 * np.pi * np.dot(params[2], x))) + np.dot(params[1], np.sin(
+        2 * np.pi * np.dot(params[2], x))) + x
 
 @jit
 def f_layer(A, B, K, x):
     """ Simple fourier Layer"""
     return np.dot(A, np.cos(2 * np.pi * np.dot(K, x))) + np.dot(B, np.sin(2 * np.pi * np.dot(K, x)))
 
-
 batch_first_f_layer = vmap(f_layer, in_axes=(2, 2, None, None), out_axes=0)
 batch_last_f_layer = vmap(f_layer, in_axes=(2, 2, None, 0), out_axes=0)
-
 
 @jit
 def f_res_layer(A, B, K, x):
     """ Residual Fourier Layer """
     return np.dot(A, np.cos(2 * np.pi * np.dot(K, x))) + np.dot(B, np.sin(2 * np.pi * np.dot(K, x))) + x
 
-
-# batch_f_res_layer = vmap(f_res_layer, in_axes=(2, 2, None, 0), out_axes=0)
-
-
 @jit
 def ReLU(x):
     """ Rectified Linear Unit (ReLU) activation function """
     return np.maximum(0, x)
 
-
 @jit
 def leaky_ReLU(x):
     return np.maximum(0.3 * x, x)
-
 
 @jit
 def fully_connected_layer(params, x):
     """ Fully Connected Layer"""
     return np.dot(x, params[0].T) + params[1]
 
-
 @jit
 def fully_connected_layer_with_ReLU(params, x):
     """ Fully Connected Layer"""
     return ReLU(np.dot(x, params[0].T) + params[1])
 
-
 @jit
 def fully_connected_layer_with_leaky_ReLU(params, x):
     return leaky_ReLU(np.dot(x, params[0].T) + params[1])
-
 
 @jit
 def layer_norm(x, eps=1e-5):
@@ -96,14 +81,11 @@ def fully_connected_forward_pass(params, f_layer_accumulate_params, fnet_feature
     B_activations = fully_connected_layer(params[1][-1], B_activations)
     a = f_layer_accumulate_params
     l = fnet_features
-    # A_activations = [[A_activations[..., a[i] + j * 2 ** l[i]: a[i] + j * 2 ** l[i] + 2 ** l[i]].swapaxes(0,1) for j in range(l[i+1])] for i in range(len(l)-1)]
-    # B_activations = [[B_activations[..., a[i] + j * 2 ** l[i]: a[i] + j * 2 ** l[i] + 2 ** l[i]].swapaxes(0,1) for j in range(l[i+1])] for i in range(len(l)-1)]
     A_activations = [[A_activations[a[i] + j * 2 ** l[i]: a[i] + j * 2 ** l[i] + 2 ** l[i]] for j in range(l[i + 1])]
                      for i in range(len(l) - 1)]
     B_activations = [[B_activations[a[i] + j * 2 ** l[i]: a[i] + j * 2 ** l[i] + 2 ** l[i]] for j in range(l[i + 1])]
                      for i in range(len(l) - 1)]
     return A_activations, B_activations
-
 
 @jit
 def fnet_forward_pass(A, B, K, inputs):
@@ -114,9 +96,7 @@ def fnet_forward_pass(A, B, K, inputs):
     x = f_layer(np.array(A[-1]), np.array(B[-1]), K[-1], x)
     return x
 
-
 batch_fnet_forward = vmap(fnet_forward_pass, in_axes=(None, None, None, 0), out_axes=0)
-
 
 @partial(jit, static_argnums=(3, 4,))
 def forward_pass(params, encoder, K, f_layer_accumulate_params, fnet_features, image, inputs, variation):
@@ -133,7 +113,6 @@ def forward_pass_with_code(params, K, f_layer_accumulate_params, fnet_features, 
     A, B = fully_connected_forward_pass(params, f_layer_accumulate_params, fnet_features, z)
     x = batch_fnet_forward(A, B, K, inputs)
     return 2 * jax.nn.sigmoid(x) - 1  #### sig
-
 
 # Make a batched version of the `forward_pass` function
 batch_forward = vmap(forward_pass, in_axes=(None, None, None, None, None, 0, None, 0), out_axes=(0, 0))
